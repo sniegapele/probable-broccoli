123b1c1f3d6d0727e535aade5eac532151847814
sniegapele <kotryna.slepetyte@gmail.com>
2021-12-04 01:09:51 +0200

    4 part - login related functions

---
+++ src/class_diagram/Prisijungimas_service.java
@@ -0,0 +1,18 @@
+package class_diagram;
+
+public class Prisijungimas_service {
+    Prisijungimo_duomenys_repository Prisijungimo_duomenys_repo;
+
+    //TODO kaip sita padaryt is viso
+    void prisijugti_su_facebook() {
+    }
+
+    void prisijungti_su_slaptažodžiu() {
+    }
+
+    void pasikeisti_slaptažodį() {
+    }
+
+    void priminti_slapažodį() {
+    }
+}
